70eac1568eb29ff504ace140371a8fa0cfaf16e2
Chris Alder <chris.alder@nanoporetech.com>
2023-12-09 11:42:32 +0000

    reverse parking

---
+++ 2023/d9/d9.py
@@ -0,0 +1,36 @@
+import numpy as np 
+import operator
+from functools import reduce
+
+test_input = """0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45"""
+
+with open("input.txt") as fh:
+    test_input = fh.read()
+
+part1 = []
+part2 = []
+for line in test_input.splitlines():
+    steps = []
+    steps_2 = []
+    current = list(map(int, line.split()))
+    while all(x == 0 for x in current) is False:
+        steps.insert(0, current[-1])
+        current = np.diff(current)
+    part1.append(sum(steps))
+
+print(sum(part1))
+
+
+part2 = []
+for line in test_input.splitlines():
+    steps = []
+    current = list(map(int, line.split()))
+    current.reverse()
+    while all(x == 0 for x in current) is False:
+        steps.insert(0, current[-1])
+        current = np.diff(current)
+    part2.append(sum(steps))
+
+print(sum(part2))
