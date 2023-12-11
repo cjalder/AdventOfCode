6437c8cab29decc825198fc1f36dbf5b93eb1a99
Chris Alder <chris.alder@nanoporetech.com>
2023-12-11 21:02:07 +0000

    lumpy numpy

--- 2023/d11/d11.py
+++ 2023/d11/d11.py
@@ -13,8 +13,8 @@ test_input = """...#......
 .......#..
 #...#....."""
 
-# with open("input.txt") as fh:
-#     test_input = fh.read()
+with open("input.txt") as fh:
+    test_input = fh.read()
 
 dat = []
 for line in test_input.splitlines():
@@ -30,36 +30,26 @@ for i in range(len(universe)):
     if np.all(universe[:,i] == "."):
         cols.append(i)
 
-for i,r in enumerate(rows):
-    universe = np.insert(universe, i+r, ".", axis = 0)
-for i,c in enumerate(cols):
-    universe = np.insert(universe, i+c, ".", axis = 1)
 
 galaxies = list(combinations((zip(*np.where(universe == "#"))),2))
 
 
-part1 = []
-for (ix,iy),(jx,jy) in galaxies:
-    dist = abs(ix-jx) + abs(iy-jy)
-    part1.append(dist)
-
-print(sum(part1))
-
-# part 2 
-universe = np.matrix(dat)
-galaxies = list(combinations((zip(*np.where(universe == "#"))),2))
 expansion = 1000000
-part2 =[]
-for (ix,iy),(jx,jy) in galaxies:
-    dist = abs(ix-jx) + abs(iy-jy)
-    for i in range(*sorted([ix, jx])):
-        if i in rows:
-            dist += (expansion-1)
-    for i in range(*sorted([iy, jy])):
-        if i in cols:
-            dist += (expansion-1)
-    part2.append(dist)
-print(sum(part2))
+def galaxy_dist(galaxies, expansion):
+    distances =[]
+    for (ix,iy),(jx,jy) in galaxies:
+        dist = abs(ix-jx) + abs(iy-jy)
+        for i in range(*sorted([ix, jx])):
+            if i in rows:
+                dist += (expansion-1)
+        for i in range(*sorted([iy, jy])):
+            if i in cols:
+                dist += (expansion-1)
+        distances.append(dist)
+    return sum(distances)
+
+print(galaxy_dist(galaxies, 2))
+print(galaxy_dist(galaxies, 1000000))
 
 
 
